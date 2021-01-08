7a06e3fc45d7e7162a7116192a9cc5f26343f4dd
Dagic-zewdu <dagi.zewdu.dz@gmail.com>
2021-01-08 10:52:10 +0300

    socket io set up message complted

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ This project is developed by metrix technologies. it is the server side <br>
 programming with node.js
 </p>
 
-### npm install for installing all dependency of the project 
+### `npm install` 
 
-### nodemon serve.js start the project on the local server:1000
+for installing all dependency of the project 
 
-### huge thanks for dagic-zewdu and worash abocher special contributer
+### `nodemon serve.js`
+
+ start the project on the local server:1111
+
+### `huge thanks` 
+
+for dagic-zewdu and worash abocher special contributer
--- socket.js
+++ socket.js
@@ -2,18 +2,20 @@ const { saveMessage, getMessages, updateMessages } = require("./controllers/mess
 const { setConnection,emitConnections, allConnections } = require("./controllers/userConnection");
 const userConnection = require("./db/connectionSchema");
 
-var emp_id  
+ 
 const webSocket=io=>io.on('connection',socket=> { 
+  var emp_id 
   /**Registers new connected user to the db */ 
-      const user=async ()=>{
-     const users=await userConnection.find({emp_id,status:'connected'})
-users.legth?emitConnections(io):setConnection({emp_id,status:'connected',
-      connected_time:Date.now()},io)       
+      const user=async (emp)=>{
+        const users=await userConnection.find({emp_id:emp,status:'connected'})
+       const connectedusers=await allConnections() 
+    users.length? io.sockets.emit('users',connectedusers):   
+   setConnection({emp_id,status:'connected', connected_time:Date.now()},io)       
       } 
       
     socket.on('onConnect',data=>{
       emp_id=data.emp_id
-      emp_id?user():()=>{}
+      emp_id?user(data.emp_id):()=>{}
     })
     socket.on('users',async ()=>{
         const users=await allConnections()
@@ -22,6 +24,8 @@ users.legth?emitConnections(io):setConnection({emp_id,status:'connected',
        socket.emit("outgoing data",new Date())
      /**recieving chat from client */
 socket.on('submit',data=>{saveMessage(data,io)})
+/**typing broadcast */
+socket.on('typing',data=>io.sockets.emit('typing',{emp_id:data.emp_id}))
    /**sending chat message */
 socket.on('chat',async ()=>{
     const messages=await getMessages()
@@ -29,7 +33,7 @@ socket.on('chat',async ()=>{
 })
   /**updating chat message */
   socket.on('update',data=>updateMessages(data,io))
-       socket.on("disconnect", ()=>{
+   socket.on("disconnect", ()=>{
       emp_id?setConnection({
         emp_id,status:'disconnected',
         disconnected_time:Date.now()
