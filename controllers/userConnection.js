28d061ee2e43636afcf2acc0ecb5133cb9f798f0
Dagic-zewdu <dagi.zewdu.dz@gmail.com>
2021-02-16 19:54:55 +0300

    All Task completed with logout socket

--- controllers/userConnection.js
+++ controllers/userConnection.js
@@ -1,51 +1,53 @@
-const userConnection = require("../db/connectionSchema")
+const userConnection = require("../db/connectionSchema");
 
 /* returns array of user connection to the server with status mentioned
   in the database schema
 */
-const allConnections=async ()=>await userConnection.find({})
+const allConnections = async () => await userConnection.find({});
 /*returns array of connected users */
-const connectedUsers=async ()=>await userConnection.find({status:'connected'})
+const connectedUsers = async () =>
+  await userConnection.find({ status: "connected" });
 /**
  
 @param {*} io =>io for broadcisting  
  */
-const emitConnections=async (io)=>io.sockets.emit('users',await allConnections()) 
+const emitConnections = async (io) =>
+  io.sockets.emit("users", await allConnections());
 /**save connection to the db based on object provided
-  *@param {*} user=>an object to save 
+ *@param {*} user=>an object to save
  *  @param {*} socket=> refers the socket object to emit
  */
-const saveConnection=async (user,socket)=>{
+const saveConnection = async (user, socket) => {
   try {
-       let connection=new userConnection(user)
-    let Save=await connection.save()
-    let connected=await connectedUsers()
-console.log(connected.length+' user connected')
-  emitConnections(socket)
-}
-catch(err){
-  console.log(err)
-}
-}
+    let connection = new userConnection(user);
+    let Save = await connection.save();
+    let connected = await connectedUsers();
+    console.log(connected.length + " user connected");
+    emitConnections(socket);
+  } catch (err) {
+    console.log(err);
+  }
+};
 /**save connection to the db based on object provided
-  *@param {*} user=>an object to save 
+ *@param {*} user=>an object to save
  *  @param {*} socket=> refers the socket object to emit
  */
-const setConnection=async (user,socket)=>{
-   try{
-    const findUser=await userConnection.find({emp_id:user.emp_id})
-   if(findUser.length>0){
-      const update=await userConnection.findByIdAndUpdate(findUser[0]._id,user) 
-      let connected=await connectedUsers()
-        console.log(connected.length+' user connected')
-        emitConnections(socket)       
-   }
-   else{
-      saveConnection(user,socket) 
-   }
-} 
- catch(err){
-  console.log(err)
-   }
-}
-module.exports={setConnection,allConnections,emitConnections}
+const setConnection = async (user, socket) => {
+  try {
+    const findUser = await userConnection.find({ emp_id: user.emp_id });
+    if (findUser.length > 0) {
+      const update = await userConnection.findByIdAndUpdate(
+        findUser[0]._id,
+        user
+      );
+      let connected = await connectedUsers();
+      console.log(connected.length + " user connected");
+      emitConnections(socket);
+    } else {
+      saveConnection(user, socket);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = { setConnection, allConnections, emitConnections };
--- socket.js
+++ socket.js
@@ -1,64 +1,110 @@
-const { emitLetterrs, createLetter, editLetter, deleteLetter, getLetters } = require("./controllers/letter");
-const { saveMessage, getMessages, updateMessages, deleteMessage, deleteLetterMessage } = require("./controllers/messages");
-const { setConnection,emitConnections, allConnections } = require("./controllers/userConnection");
+const {
+  emitLetterrs,
+  createLetter,
+  editLetter,
+  deleteLetter,
+  getLetters,
+} = require("./controllers/letter");
+const {
+  saveMessage,
+  getMessages,
+  updateMessages,
+  deleteMessage,
+  deleteLetterMessage,
+} = require("./controllers/messages");
+const {
+  setConnection,
+  emitConnections,
+  allConnections,
+} = require("./controllers/userConnection");
 const userConnection = require("./db/connectionSchema");
 
- 
-const webSocket=io=>io.on('connection',socket=> { 
-  var emp_id 
-  /**Registers new connected user to the db */ 
-      const user=async (emp)=>{
-        const users=await userConnection.find({emp_id:emp,status:'connected'})
-       const connectedusers=await allConnections() 
-    users.length? io.sockets.emit('users',connectedusers):   
-   setConnection({emp_id,status:'connected', connected_time:Date.now()},io)       
-      } 
-   
-      /**onConnect save the the connected user to db */   
-  socket.on('onConnect',data=>{
-      emp_id=data.emp_id
-      emp_id?user(data.emp_id):()=>{}
-    })
-    socket.on('users',async ()=>{
-        const users=await allConnections()
-        io.sockets.emit('users',users)
-    }) 
-       socket.emit("outgoing data",new Date())
-    
-       /**recieving chat from client */
-socket.on('submit',data=>{saveMessage(data,io)})  //for saving message
-socket.on('update',data=>updateMessages(data,io))  //for updating message
-socket.on('delete',data=>deleteMessage(data,io))    // for deleting message
-socket.on('delete_letter_message',data=>deleteLetterMessage(data,io)) // for deleting message
-/**typing broadcast */
-socket.on('typing',data=>io.sockets.emit('typing',{emp_id:data.emp_id}))
-socket.on('typing_letter',data=>io.sockets.emit('typing_letter',{emp_id:data.emp_id}))   
+const webSocket = (io) =>
+  io.on("connection", (socket) => {
+    var emp_id;
+    /**Registers new connected user to the db */
+    const user = async (emp) => {
+      const users = await userConnection.find({
+        emp_id: emp,
+        status: "connected",
+      });
+      const connectedusers = await allConnections();
+      users.length
+        ? io.sockets.emit("users", connectedusers)
+        : setConnection(
+            { emp_id, status: "connected", connected_time: Date.now() },
+            io
+          );
+    };
 
-/**sending chat message */
-socket.on('chat',async ()=>{
-    const messages=await getMessages()
-    io.sockets.emit('chat',messages)
-})
- 
-/**updating chat message */
-  socket.on('update',data=>updateMessages(data,io))
-  
-  /**letter on socket connection */
-   socket.on('letters',async ()=>{
-     const letters=await getLetters()
-     io.sockets.emit('letters',letters)
-   }) 
-   socket.on('create_letter',data=>createLetter(data,io)) //create letter
-   socket.on('update_letter',data=>editLetter(data,io))  //update letter
-   socket.on('delete_letter',data=>deleteLetter(data,io)) //delete letter
-  
-   /**update db while user is diconnected */ 
-  socket.on("disconnect", ()=>{
-      emp_id?setConnection({
-        emp_id,status:'disconnected',
-        disconnected_time:Date.now()
-        },io):()=>{}
+    /**onConnect save the the connected user to db */
+    socket.on("onConnect", (data) => {
+      emp_id = data.emp_id;
+      emp_id ? user(data.emp_id) : () => {};
     });
-   
-      })
-module.exports={webSocket}      
+    socket.on("users", async () => {
+      const users = await allConnections();
+      io.sockets.emit("users", users);
+    });
+    socket.emit("outgoing data", new Date());
+
+    /**recieving chat from client */
+    socket.on("submit", (data) => {
+      saveMessage(data, io);
+    }); //for saving message
+    socket.on("update", (data) => updateMessages(data, io)); //for updating message
+    socket.on("delete", (data) => deleteMessage(data, io)); // for deleting message
+    socket.on("delete_letter_message", (data) => deleteLetterMessage(data, io)); // for deleting message
+    /**typing broadcast */
+    socket.on("typing", (data) =>
+      io.sockets.emit("typing", { emp_id: data.emp_id })
+    );
+    socket.on("typing_letter", (data) =>
+      io.sockets.emit("typing_letter", { emp_id: data.emp_id })
+    );
+
+    /**sending chat message */
+    socket.on("chat", async () => {
+      const messages = await getMessages();
+      io.sockets.emit("chat", messages);
+    });
+
+    /**updating chat message */
+    socket.on("update", (data) => updateMessages(data, io));
+
+    /**letter on socket connection */
+    socket.on("letters", async () => {
+      const letters = await getLetters();
+      io.sockets.emit("letters", letters);
+    });
+    socket.on("create_letter", (data) => createLetter(data, io)); //create letter
+    socket.on("update_letter", (data) => editLetter(data, io)); //update letter
+    socket.on("delete_letter", (data) => deleteLetter(data, io)); //delete letter
+
+    /**update db while user is diconnected */
+    socket.on("disConnect", () => {
+      emp_id
+        ? setConnection(
+            {
+              emp_id,
+              status: "disconnected",
+              disconnected_time: Date.now(),
+            },
+            io
+          )
+        : () => {};
+    });
+    socket.on("disconnect", () => {
+      emp_id
+        ? setConnection(
+            {
+              emp_id,
+              status: "disconnected",
+              disconnected_time: Date.now(),
+            },
+            io
+          )
+        : () => {};
+    });
+  });
+module.exports = { webSocket };
